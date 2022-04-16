f7fb19b6aa4a7fe4fd8dd0d289188377cdd65727
SHIMANTO50 <shimantotsp@gmail.com>
2022-04-16 22:52:21 +0600

    route setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home/Home/Home';
+import About from './Pages/About/About';
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+      </Routes>
     </div>
   );
 }
