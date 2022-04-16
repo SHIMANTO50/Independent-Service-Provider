2d2620c3a983b725cc8ed442ed85f5b602447eba
SHIMANTO50 <shimantotsp@gmail.com>
2022-04-17 04:53:21 +0600

    banner added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
 import DiscountBatch from '../DiscountBatch/DiscountBatch';
 import Services from './../Services/Services';
 
 const Home = () => {
     return (
         <div>
+            <Banner></Banner>
             <Services></Services>
             <DiscountBatch></DiscountBatch>
         </div>
