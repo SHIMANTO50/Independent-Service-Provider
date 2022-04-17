4dca430e07bdbeb4d3beb3a694cab9348b8eba5d
SHIMANTO50 <shimantotsp@gmail.com>
2022-04-17 09:56:40 +0600

    Footer Added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 const Footer = () => {
+    const today = new Date();
+    const year = today.getFullYear();
     return (
-        <footer>
-            <p><small>Copyright &copy;</small></p>
+
+        <footer className='mt-3 bg-primary'>
+            <p className='text-center mt-3 tex-white p-5'><small>Copyright &copy; {year}</small></p>
         </footer>
     );
 };
