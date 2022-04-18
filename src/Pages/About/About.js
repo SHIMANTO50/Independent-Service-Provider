19faadd17cf09e498630317e4249f84564b734fc
SHIMANTO50 <shimantotsp@gmail.com>
2022-04-18 09:29:34 +0600

    About me section added

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import './About.css';
 
 const About = () => {
     return (
         <div>
-            <h2>This is About Us</h2>
+            <div className='about-me d-flex align-items-center w-100'>
+                <div className='container'>
+                    <div className="row">
+                        <div className="col-md-12 text-center w-50 mx-auto m-5">
+                            <h2>About Me</h2>
+                            <h4>I am Tanjimul Hasan. My Profession is Teaching. I have experience of 5 years. I I focused on 3 subjects. These Subjects are Physics,Chemistry and Higher Math. My main goal is to make the topic easier to student. So that student get interested in this subjects.</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
